f94012308adee3429030f0de70b943125ae82880
GabrielAndradeFerreira <80432575+GabrielAndradeFerreira@users.noreply.github.com>
2021-06-09 08:16:56 -0300

    Add files via upload

---
+++ CalcAreaQuadrado/Calcular Area do Quadrado.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+  float e_lado, s_area;//Declaração das variaveis.
+
+  scanf("%f",&e_lado);//Le dados do teclado.
+
+  s_area = e_lado*e_lado;//Executo o processo.
+
+  printf("%f",s_area);//mostra resultaddo na tela.
+
+  
+  return 0;
+}
