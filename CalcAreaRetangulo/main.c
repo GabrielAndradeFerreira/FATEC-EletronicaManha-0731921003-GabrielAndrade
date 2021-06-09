d04a10bb430493e33161699ca1835a999c91cf1f
GabrielAndradeFerreira <80432575+GabrielAndradeFerreira@users.noreply.github.com>
2021-06-09 08:33:28 -0300

    Create main.c

---
+++ CalcAreaRetangulo/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+  
+  int e_Lum, e_Ldois, s_area;//Declaração de variaveis.
+
+  scanf("%d",&e_Ldois);//Atribuição de valores as variaveis.
+  scanf("%d",&e_Lum);
+  
+  s_area = e_Lum*e_Ldois;//Execução do processo.
+
+  printf("%d",s_area);//Mostra o resultado ao usuario.
+  
+  return 0;
+}
